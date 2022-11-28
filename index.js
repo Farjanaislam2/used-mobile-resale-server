68f0ac5bc2dd4a820c08d1ed11fcf0f2e05d2387
Farjana Islam <farjanaanika2@gmail.com>
2022-11-28 08:32:39 -0800

    last

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ try{
     })
 
     //load category products
-    app.get('/categories/:id', async(req,res)=>{
+    app.get('/singlecategories/:id', async(req,res)=>{
         const id = req.params.id;
         const query = {_id:ObjectId(id)}
         const products = await categoryCollection.findOne(query);
